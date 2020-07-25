ef7c5c4a64680fa1e73871436156932a0a011e15
shashankdinesh <guptashashank89@yahoo.com>
2020-07-25 10:57:03 +0530

    updated

---
+++ titanic_dataset/titanic_repo.R
@@ -0,0 +1,7 @@
+# Load packages
+library('ggplot2') # visualization
+library('ggthemes') # visualization
+library('scales') # visualization
+library('dplyr') # data manipulation
+library('mice') # imputation
+library('randomForest') # classification algorithm
